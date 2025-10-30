3d27c77a8f6749d3ee8369003221a402e7f269d6
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2025-10-30 23:46:29 +0530

    Update ChatWindow.tsx

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -12,6 +12,7 @@ interface ChatWindowProps {
 
 
 
+
 const ChatWindow: React.FC<ChatWindowProps> = ({ messages, isTyping }) => {
   return (
     <div className="flex-1 overflow-y-auto p-4 bg-white">
